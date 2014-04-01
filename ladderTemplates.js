370f3f70ea957cc7ed0286ebf40e71f0d91ee49f
Joshua Ohlman <codewriterohs@gmail.com>
2014-03-31 19:31:28 -0500

    refactor ladderTemplates.js to use new inheritance paradigm

--- ladderTemplates.js
+++ ladderTemplates.js
@@ -1,90 +1,101 @@
-var LadderTemplate = function (config) {
-    for (var prop in this) {
-        if (this[prop] instanceof Array) {
-            // Array properties are references, we'll duplicate them here so we can modify them without affecting the prototype versions.
-            this[prop] = this[prop].concat();
+var LadderTemplate = Ladder.inherit();
+
+LadderTemplate.attach("elementType", function () {
+    return "div";
+});
+
+LadderTemplate.attach("valueBinding", function () {
+    return "text";
+});
+
+LadderTemplate.attach("valueSource", function () {
+    return "JSON.stringify($data)";
+});
+
+LadderTemplate.attach("otherBindings", function () {
+    return [];
+});
+
+LadderTemplate.attach("cssBindings", function () {
+    return [];
+});
+
+LadderTemplate.attach("css", function () {
+    return [];
+});
+
+LadderTemplate.attach("allBindings", function () {
+    return function () {
+         var bindings = [];
+        if (this.valueBinding && this.valueSource) bindings.push(this.valueBinding + ": " + this.valueSource);
+        if (this.cssBindings && this.cssBindings.length) bindings.push("css: {" +
+            this.cssBindings.filter(function (a) {
+                return a;
+            }).join(", ") 
+        + "}");
+        if (this.otherBindings) this.otherBindings.filter(function (a) {
+            return a;
+        }).forEach(function (a) {
+            bindings.push(a);
+        });
+        return bindings;
+    };
+});
+
+LadderTemplate.attach("allAttributes", function () {
+    return function() {
+        var attributes = [];
+        if (this.css) attributes.push({
+            "name": "class",
+            "value": this.css.join(" ")
+        });
+        if (this.attributes) this.attributes.filter(function (a) {
+            return a && a.name;
+        }).forEach(function (a) {
+            attributes.push(a);
+        });
+        return attributes;
+    };
+});
+LadderTemplate.attach("renderVirtual", function () {
+    return function () {
+        var startTag = "<!-- ko " + this.allBindings().join(", ") + " -->";
+        var centerTag = "";
+        if (this.innerTemplate) {
+            centerTag = $('<div>').append(this.innerTemplate.render()).html();
         }
-    }
-    if (typeof config == "function") config(self);
-    else {
-        for (var prop in config) {
-            if (config.hasOwnProperty(prop)) this[prop] = config[prop];
+        var endTag = "<!-- /ko -->";
+        return $(startTag + centerTag + endTag);
+    };
+});
+LadderTemplate.attach("render", function () {
+    return function () {
+        if (this.elementType == "virtual") return this.renderVirtual();
+        if (this.templateSource) return $(this.templateSource);
+        var result = $("<" + this.elementType + ">"),
+            bindings = this.allBindings(),
+            attributes = this.allAttributes()
+            ;
+        result.attr('data-bind', bindings.join(", "));
+        attributes.forEach(function (a) {
+            result.attr(a.name, a.value);
+        });
+        var inner = this.innerTemplate;
+        if (typeof inner == "function") inner = this.innerTemplate();
+        if (inner instanceof LadderTemplate) {
+            result.append(inner.render());
+        } else if (inner) {
+            throw new Error("innerTemplate must be an instance of LadderTemplate");
         }
-    }
-};
+        return result;
+    };
+});
 
 LadderTemplate.defaultRender = new LadderTemplate({
     elementType: 'virtual',
     valueSource: "$data.template",
     valueBinding: "render"
 });
-
-// Default values, specified on the prototype level
-LadderTemplate.prototype.elementType = "div";
-LadderTemplate.prototype.valueBinding = "text";
-LadderTemplate.prototype.valueSource = "JSON.stringify($data)";
-LadderTemplate.prototype.otherBindings = [];
-LadderTemplate.prototype.cssBindings = [];
-LadderTemplate.prototype.css = [];
-
-LadderTemplate.prototype.allBindings = function() {
-    var bindings = [];
-    if (this.valueBinding && this.valueSource) bindings.push(this.valueBinding + ": " + this.valueSource);
-    if (this.cssBindings && this.cssBindings.length) bindings.push("css: {" +
-        this.cssBindings.filter(function (a) {
-            return a;
-        }).join(", ") 
-    + "}");
-    if (this.otherBindings) this.otherBindings.filter(function (a) {
-        return a;
-    }).forEach(function (a) {
-        bindings.push(a);
-    });
-    return bindings;
-};
-
-LadderTemplate.prototype.allAttributes = function() {
-    var attributes = [];
-    if (this.css) attributes.push({
-        "name": "class",
-        "value": this.css.join(" ")
-    });
-    if (this.attributes) this.attributes.filter(function (a) {
-        return a && a.name;
-    }).forEach(function (a) {
-        attributes.push(a);
-    });
-    return attributes;
-};
-LadderTemplate.prototype.renderVirtual = function () {
-    var startTag = "<!-- ko " + this.allBindings().join(", ") + " -->";
-    var centerTag = "";
-    if (this.innerTemplate) {
-        centerTag = $('<div>').append(this.innerTemplate.render()).html();
-    }
-    var endTag = "<!-- /ko -->";
-    return $(startTag + centerTag + endTag);
-}
-LadderTemplate.prototype.render = function () {
-    if (this.elementType == "virtual") return this.renderVirtual();
-    if (this.templateSource) return $(this.templateSource);
-    var result = $("<" + this.elementType + ">"),
-        bindings = this.allBindings(),
-        attributes = this.allAttributes()
-        ;
-    result.attr('data-bind', bindings.join(", "));
-    attributes.forEach(function (a) {
-        result.attr(a.name, a.value);
-    });
-    var inner = this.innerTemplate;
-    if (typeof inner == "function") inner = this.innerTemplate();
-    if (inner instanceof LadderTemplate) {
-        result.append(inner.render());
-    } else if (inner) {
-        throw new Error("innerTemplate must be an instance of LadderTemplate");
-    }
-    return result;
-};
 
 ko.bindingHandlers.render = {
     init: function (element, valueAccessor) {
